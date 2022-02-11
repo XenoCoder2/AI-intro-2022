bd50a388edb4492e45eafb94a3afe9abd0f44e69
XenoCoder2 <brandonewer17951@gmail.com>
2022-02-11 13:25:30 +1100

    Changes to AI Movement Script and Main Scene

--- Assets/Scripts/AIMovement.cs
+++ Assets/Scripts/AIMovement.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class AIMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Create the variable and place data in the variable, use the variable. :)
+    public GameObject position0;
+    public GameObject position1;
+    
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        Vector2 aiTransform = transform.position;
+        //transform.position = Vector2.MoveTowards(transform.position, position1.transform.position, Time.deltaTime);
+        /*if (transform.position.x < position1.transform.position.x)
+        {
+            //Move right
+            
+            aiTransform.x += 1 * Time.deltaTime * 3;
+            transform.position = aiTransform;
+
+        }
+        else 
+        {
+            aiTransform.x -= 1 * Time.deltaTime * 3;
+            transform.position = aiTransform;
+        }
+
+        if (transform.position.y < position1.transform.position.y)
+         {
+             transform.position += Vector3.up * 1 * Time.deltaTime;
+         }    
+         else
+         {
+             transform.position -= Vector3.up * 1 * Time.deltaTime;
+         }
+        */
+
+        Vector2 directionToPos1 = position1.transform.position - transform.position;
+        directionToPos1.Normalize();
+        transform.position += (Vector3) directionToPos1 * 1 * Time.deltaTime; 
     }
 }
